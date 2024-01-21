736ac6776158242196f3a9e6bf6cef148615ff66
Harsha199 <Optimist.Coder19@gmail.com>
2024-01-21 12:36:16 +0530

    Hacking using Ui's
    making easy and understandable CSS structures  and usage of css easy manner with animated reponses by which we can copy the code and use it in further websites

---
+++ hacking/src/index.js
@@ -0,0 +1,20 @@
+
+
+
+// Add the Font Awesome brand icons to the library
+
+
+import React from 'react'
+import { hydrate, render } from 'react-dom'
+import './index.scss';
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
+
+serviceWorker.unregister()
